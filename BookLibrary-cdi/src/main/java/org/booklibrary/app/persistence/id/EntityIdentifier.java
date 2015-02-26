1c14e9422e5c075902d71854178f584ac299b9a0
ykop <e.kopaev@gmail.com>
2015-02-26 18:16:25 +0200

    refactor entity id class

--- BookLibrary-cdi/src/main/java/org/booklibrary/app/persistence/id/EntityIdentifier.java
+++ BookLibrary-cdi/src/main/java/org/booklibrary/app/persistence/id/EntityIdentifier.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
 import java.util.Arrays;
 
 /**
- * Entity Id is a Universally Unique Identified (UUID, aka GUID), used as the primary key for all
+ * Entity Identifier is a Universally Unique Identified (UUID, aka GUID), used as the primary key for all
  * entities. It can be represented as a 32 character hex string, e.g. "d91caf4de37640faa59602c42eecf237".
  * The class is Embeddable, and should be used with the EmbeddedId
  * annotation.
@@ -17,46 +17,47 @@ import java.util.Arrays;
 @Embeddable
 public class EntityIdentifier implements Serializable {
 
-	/**
-	 * The unique entity id, set to a random UUID.
-	 */
-	@Column(name = "ID", length = IdGenerator.NUM_UUID_BYTES,
-			columnDefinition = "BINARY(16)")
-	private byte[] id;
+    /**
+     * The unique entity id, set to a random UUID.
+     */
+    @Column(name = "ID",
+            length = IdGenerator.NUM_UUID_BYTES,
+            columnDefinition = "BINARY(16)")
+    private byte[] id;
 
-	public EntityIdentifier() {
-		this.id = IdGenerator.generateUUID();
-	}
+    public EntityIdentifier() {
+        this.id = IdGenerator.generateUUID();
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hashCode(id);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (!(obj instanceof EntityIdentifier)) {
-			return false;
-		}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof EntityIdentifier)) {
+            return false;
+        }
 
-		EntityIdentifier other = (EntityIdentifier) obj;
+        final EntityIdentifier other = (EntityIdentifier) obj;
 
-		return Arrays.equals(this.id, other.id);
-	}
+        return Arrays.equals(this.id, other.id);
+    }
 
-	@Override
-	public String toString() {
-		return Hex.encodeHexString(this.id);
-	}
+    @Override
+    public String toString() {
+        return Hex.encodeHexString(this.id);
+    }
 
-	public byte[] getId() {
-		return this.id;
-	}
+    public byte[] getId() {
+        return this.id;
+    }
 
 }
