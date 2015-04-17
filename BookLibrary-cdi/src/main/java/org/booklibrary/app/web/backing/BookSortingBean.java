4f067dcffdfd93db707a0a6bbf2556da16a7afc2
ykop <e.kopaev@gmail.com>
2015-04-17 09:37:59 +0300

    add sorting functionality + new view style

---
+++ BookLibrary-cdi/src/main/java/org/booklibrary/app/web/backing/BookSortingBean.java
@@ -0,0 +1,75 @@
+package org.booklibrary.app.web.backing;
+
+import org.richfaces.component.SortOrder;
+
+import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+
+@Named
+@ViewScoped
+public class BookSortingBean implements Serializable {
+
+    private static final String SORT_PROPERTY_PARAMETER = "sortProperty";
+
+    private static final String CREATED_SORT_OPTION_KEY = "created";
+    private static final String RATING_SORT_OPTION_KEY = "rating";
+
+
+    private SortOrder createdDefaultSortOrder = SortOrder.descending;
+    private SortOrder ratingDefaultSortOrder = SortOrder.descending;
+
+    private Map<String, SortOrder> sortsOrders;
+    private Queue<String> sortPriorities;
+
+    @Inject
+    private FacesContext facesContext;
+
+    public BookSortingBean() {
+
+    }
+
+    @PostConstruct
+    public void initSortOrders() {
+
+        this.sortsOrders = new HashMap<>();
+        this.sortPriorities = new ArrayDeque<>();
+        // set common sorting priorities
+        this.sortPriorities.add(CREATED_SORT_OPTION_KEY);
+        this.sortPriorities.add(RATING_SORT_OPTION_KEY);
+        this.sortsOrders.put(CREATED_SORT_OPTION_KEY, createdDefaultSortOrder);
+        this.sortsOrders.put(RATING_SORT_OPTION_KEY, ratingDefaultSortOrder);
+    }
+
+    public void sort() {
+        String property = this.facesContext.getExternalContext().getRequestParameterMap()
+                .get(SORT_PROPERTY_PARAMETER);
+        if (property != null) {
+            SortOrder currentPropertySortOrder = this.sortsOrders.get(property);
+            initSortOrders();
+            if (!sortPriorities.contains(property)) {
+                this.sortPriorities.add(property);
+            }
+            if (currentPropertySortOrder == null || currentPropertySortOrder.equals(SortOrder.descending)) {
+                this.sortsOrders.put(property, SortOrder.ascending);
+            } else {
+                this.sortsOrders.put(property, SortOrder.descending);
+            }
+        }
+    }
+
+    public Map<String, SortOrder> getSortsOrders() {
+        return sortsOrders;
+    }
+
+    public Queue<String> getSortPriorities() {
+        return sortPriorities;
+    }
+}
